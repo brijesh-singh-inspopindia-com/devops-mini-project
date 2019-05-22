d40f34837f850672b586bba9105dae2347841c51
brijesh-singh-inspopindia-com <50905934+brijesh-singh-inspopindia-com@users.noreply.github.com>
2019-05-22 20:30:05 +0530

    Update configure_aws_cli.sh

--- configure_aws_cli.sh
+++ configure_aws_cli.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-aws configure set aws_access_key_id AKIAJ4JKUQXPE5Y343XA 
-aws configure set aws_secret_access_key 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 
+aws configure set aws_access_key_id AKIAJH3GDBEIMWCZVPPA 
+aws configure set aws_secret_access_key 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 
 aws configure set region us-east-1
 aws configure set output json
 aws --version
