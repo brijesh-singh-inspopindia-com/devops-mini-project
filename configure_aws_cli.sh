e7bfcad3cd062d5c62482e6281731a50560c70dd
brijesh-singh-inspopindia-com <50905934+brijesh-singh-inspopindia-com@users.noreply.github.com>
2019-05-23 00:51:57 +0530

    Delete configure_aws_cli.sh

--- configure_aws_cli.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-aws configure set aws_access_key_id AKIA37YKITATWUZYO2QZ 
-aws configure set aws_secret_access_key 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 
-aws configure set region us-east-1
-aws configure set output json
-aws --version
