bed1174e628f6f15e86ebefa4e70c10aea2cb9e5
brijesh-singh-inspopindia-com <50905934+brijesh-singh-inspopindia-com@users.noreply.github.com>
2019-05-22 23:52:55 +0530

    Update launch_ec2_instance.sh

--- launch_ec2_instance.sh
+++ launch_ec2_instance.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #AWS variable
-vpc_id="vpc-7aa17d00"
-sub_id="subnet-56b43b78"
-route_table="rtb-8a2b70f5"
-internet_gateway="igw-3246454a"
-sec_id="sg-0a13bde6a33dbbfa3"
+vpc_id="vpc-09cb716cf84db6c81"
+sub_id="subnet-095879cc20ee6471b"
+route_table="rtb-093c26836f0a1e06c"
+internet_gateway="igw-082caddba2205b5b9"
+sec_id="sg-0a0a7ff9c6298d97d"
 aws_image_id="ami-0a313d6098716f372"
 i_type="t2.micro"
 tag="devops-demo-instance"
@@ -13,34 +13,21 @@ aws_key_name="devops-key"
 ssh_key="devops-key.pem"
 uid=$RANDOM
 
-# Generate AWS Keys and store in this local box
-#echo -e "\e[32m Generating key Pairs\033[0m"
-#aws ec2 create-key-pair --key-name devenv-key --query 'KeyMaterial' --output text 2>&1 | tee $ssh_key
-
-#Set read only access for key
-#echo "Setting permissions"
-#chmod 400 $ssh_key
+allocationid_ec2= $(aws ec2 allocate-address --domain vpc | grep AllocationId)
 
 echo "Creating EC2 instance in AWS"
 
 ec2_id=$(aws ec2 run-instances --image-id $aws_image_id --count 1 --region us-east-1 --instance-type $i_type --key-name $aws_key_name --security-group-ids $sec_id --subnet-id $sub_id --associate-public-ip-address  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$tag - $uid},{Key=WatchTower,Value=$tag},{Key=AutomatedID,Value=$uid}]" | grep InstanceId | cut -d":" -f2 | cut -d'"' -f2)
 
-
-# Log date, time, random ID. This may come handy in the future for troubleshooting
-date >> logs.txt
-#pwd >> logs.txt
-echo $ec2_id >> logs.txt
-echo ""
+aws ec2 associate-address --instance-id $ec2_id --allocation-id $allocationid_ec2
 
 echo -e "\t\033[0;31mEC2 Instance ID: $ec2_id\033[0m"
-#echo "Unique ID: $uid"
+
 elastic_ip=$(aws ec2 describe-instances --instance-ids $ec2_id --query 'Reservations[0].Instances[0].PublicIpAddress' | cut -d'"' -f2)
 echo -e "\t \033[0;31mElastic IP: $elastic_ip\033[0m"
-echo $elastic_ip >> logs.txt
-echo "=====" >> logs.txt
 
 echo ""
-countdown_timer=60
+countdown_timer=180
 echo -e "\e[32m Please wait while your instance is being powered on..We are trying to ssh into the EC2 instance\033[0m"
 echo -e "\e[32m Copy/paste the below command to acess your EC2 instance via SSH from this machine. You may need this later.\033[0m"
 echo ""
